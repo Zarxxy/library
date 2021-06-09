feacb85899d03a6a30933bf62289d672429b3964
Rene <rene.zobus@gmail.com>
2021-06-09 09:31:40 +0200

    Added comments & updated css on the table

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-
+//Global variables
+let myLibrary = [];
 const tableWithBooks = document.querySelector(".lib");
 
+//Button variables
 const btnToOpenNewBookForm = document.querySelector(".btnAdd");
 const FormForNewBook = document.querySelector(".newBookForm");
 const btnToConfirmNewBook = document.querySelector(".btnConfirm");
 
+//Event listener for buttons
 btnToOpenNewBookForm.addEventListener("click", () => {
     openTheForm();
 })
@@ -13,8 +16,7 @@ btnToConfirmNewBook.addEventListener("click", () => {
     closeTheForm();
 })
 
-let myLibrary = [];
-
+//Constructor for book object
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -22,11 +24,13 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
+//Adds new book objects into the myLibrary array
 function addBookToLibrary(book) {    
     myLibrary.push(book);
 }
 
-function printLibrary(){
+//Creates and renders the table, with the books, from the myLibrary array
+function renderLibrary(){
     while(tableWithBooks.firstChild) tableWithBooks.removeChild(tableWithBooks.firstChild);
     let i = 0
     myLibrary.forEach(book => {
@@ -56,10 +60,12 @@ function printLibrary(){
     })))
 }
 
+//Opens the form to add new books
 function openTheForm() {
     FormForNewBook.style.display = "flex";
 }
 
+//Closes the form to add new books, creates a new book object from the user input and resets the form  
 function closeTheForm() {
     let title = document.querySelector("#inputTitle").value;
     let author = document.querySelector("#inputAuthor").value;
@@ -69,7 +75,7 @@ function closeTheForm() {
     const NewBook = new Book (title, author, pages, read)
 
     addBookToLibrary(NewBook);
-    printLibrary();
+    renderLibrary();
 
     document.querySelector("#inputTitle").value = "";
     document.querySelector("#inputAuthor").value = "";
@@ -79,11 +85,12 @@ function closeTheForm() {
     FormForNewBook.style.display = "none";
 }
 
+//Removes a book from the Array and table
 function removeBook(i){
     let elementToRemove = document.querySelector(`[data-index= "${i}"]`);
     elementToRemove.remove();
     myLibrary.splice(i, 1);
-    printLibrary();
+    renderLibrary();
     console.log(myLibrary)
 }
 
@@ -94,4 +101,4 @@ const dracula = new Book("Dracula", "Bram Stoker", "592 pages", "yes")
 addBookToLibrary(theHobbit);
 addBookToLibrary(mobyDick);
 addBookToLibrary(dracula);
-printLibrary();
+renderLibrary();
